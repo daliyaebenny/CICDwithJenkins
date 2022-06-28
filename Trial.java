58e7891ae4f0a2eceb9ecf4d6d125e74bfa72999
Daliya Benny <daliyaebenny@gmail.com>
2022-06-28 11:42:54 -0700

    Testing CI

--- Trial.java
+++ Trial.java
@@ -1,5 +1,6 @@
 public class Trial {
     public static void main(String[] args) {
         System.out.println("Simple demonstration for continuous integration");
+        System.out.println("testing CI commit commit 1");
     }
 }
